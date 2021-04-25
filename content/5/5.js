6ef5498f29be8bdf840084f8315362e20d0007c2
Pavel <52429594+pagamov@users.noreply.github.com>
2021-04-25 08:41:15 +0300

    5/5

--- content/5/5.js
+++ content/5/5.js
@@ -1,14 +1,17 @@
 cross = new Image();
 cross.src = document.getElementById('file2').value;
 
+// cross.src = '..'
+
 files_arr = JSON.parse(document.getElementById('file1').value); //arr for path and flag
+// files_arr =
 curr_idx = files_arr.current_idx;
 paths = files_arr.paths;
 
 base_image = new Image();
 base_image.src = paths[curr_idx];
 
-var index_of_max = document.getElementById('sasha');
+var index_of_max = document.getElementById('counter');
 index_of_max.value = curr_idx.toString() + '/' + (paths.length-1).toString();
 
 function make_base() {
@@ -26,10 +29,7 @@ function make_base() {
 make_base();
 
 let isDrawing = false;
-let x = 0;
-let y = 0;
-let mode = 'to_draw'
-var razmetka = []
+let x = 0, y = 0, mode = 'to_draw', razmetka = []
 const myPics = document.getElementById('canvas');
 const context = myPics.getContext('2d');
 
@@ -42,21 +42,23 @@ myPics.addEventListener('mousedown', e => {
 });
 
 myPics.addEventListener('mousemove', e => {
-    if (isDrawing === true) {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-        context.beginPath();
-        context.drawImage(base_image, 0, 0);
-        for (var i = 0; i < razmetka.length; i++) {
-            context.strokeRect(razmetka[i].x1, razmetka[i].y1, razmetka[i].x2-razmetka[i].x1, razmetka[i].y2-razmetka[i].y1);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.beginPath();
+    context.drawImage(base_image, 0, 0);
+    if (paths[curr_idx] in razmetka) {
+        var raz_p = razmetka[paths[curr_idx]]
+        for (var i = 0; i < raz_p.length; i++) {
+            context.strokeRect(raz_p[i].x1, raz_p[i].y1, raz_p[i].x2-raz_p[i].x1, raz_p[i].y2-raz_p[i].y1);
         }
+    }
+    if (isDrawing === true) {
         context.strokeRect(x, y, e.offsetX-x, e.offsetY-y);
     } else if (mode === 'to_delete') {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-        context.beginPath();
-        context.drawImage(base_image, 0, 0);
-        for (var i = 0; i < razmetka.length; i++) {
-            context.strokeRect(razmetka[i].x1, razmetka[i].y1, razmetka[i].x2-razmetka[i].x1, razmetka[i].y2-razmetka[i].y1);
-            context.drawImage(cross, razmetka[i].x2, razmetka[i].y1-40);
+        if (paths[curr_idx] in razmetka) {
+            var raz_p = razmetka[paths[curr_idx]]
+            for (var i = 0; i < razmetka.length; i++) {
+                context.drawImage(cross, raz_p[i].x2, raz_p[i].y1-40);
+            }
         }
     }
 });
@@ -66,29 +68,34 @@ window.addEventListener('mouseup', e => {
         isDrawing = false;
         tag = document.getElementById('defect_option').value;
         if (tag != '') {
-            if (y > e.offsetY && x > e.offsetX) {
-                razmetka.push({tag:tag,x1:e.offsetX,y1:e.offsetY,x2:x,y2:y})
-            } else if (y > e.offsetY) {
-                razmetka.push({tag:tag,x1:x,y1:e.offsetY,x2:e.offsetX,y2:y})
-            } else if (x > e.offsetX) {
-                razmetka.push({tag:tag,x1:e.offsetX,y1:y,x2:x,y2:e.offsetY})
-            } else {
-                razmetka.push({tag:tag,x1:x,y1:y,x2:e.offsetX,y2:e.offsetY})
+            if (paths[curr_idx] in razmetka) {
+                if (y > e.offsetY && x > e.offsetX) {
+                    razmetka[paths[curr_idx]].push({tag:tag,x1:e.offsetX,y1:e.offsetY,x2:x,y2:y})
+                } else if (y > e.offsetY) {
+                    razmetka[paths[curr_idx]].push({tag:tag,x1:x,y1:e.offsetY,x2:e.offsetX,y2:y})
+                } else if (x > e.offsetX) {
+                    razmetka[paths[curr_idx]].push({tag:tag,x1:e.offsetX,y1:y,x2:x,y2:e.offsetY})
+                } else {
+                    razmetka[paths[curr_idx]].push({tag:tag,x1:x,y1:y,x2:e.offsetX,y2:e.offsetY})
+                }
             }
         }
         console.log(razmetka)
     } else if (mode === 'to_delete') {
-        for (var i = 0; i < razmetka.length; i++) {
-            if (e.offsetX < razmetka[i].x2+40 && e.offsetX > razmetka[i].x2 && e.offsetY < razmetka[i].y1 && e.offsetY > razmetka[i].y1-40) {
-                razmetka.splice(i,1);
-                context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-                context.beginPath();
-                context.drawImage(base_image, 0, 0);
-                for (var i = 0; i < razmetka.length; i++) {
-                    context.strokeRect(razmetka[i].x1, razmetka[i].y1, razmetka[i].x2-razmetka[i].x1, razmetka[i].y2-razmetka[i].y1);
-                    context.drawImage(cross, razmetka[i].x2, razmetka[i].y1-40);
+        if (paths[curr_idx] in razmetka) {
+            var raz_p = razmetka[paths[curr_idx]]
+            for (var i = 0; i < raz_p.length; i++) {
+                if (e.offsetX < raz_p[i].x2+40 && e.offsetX > raz_p[i].x2 && e.offsetY < raz_p[i].y1 && e.offsetY > raz_p[i].y1-40) {
+                    razmetka[paths[curr_idx]].splice(i,1);
+                    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+                    context.beginPath();
+                    context.drawImage(base_image, 0, 0);
+                    for (var i = 0; i < razmetka.length; i++) {
+                        context.strokeRect(raz_p[i].x1, raz_p[i].y1, raz_p[i].x2-raz_p[i].x1, raz_p[i].y2-raz_p[i].y1);
+                        context.drawImage(cross, raz_p[i].x2, raz_p[i].y1-40);
+                    }
+                    break
                 }
-                break
             }
         }
     }
